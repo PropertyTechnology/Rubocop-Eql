e1f44caa8543d56cbad92d7a707b02de3ca682a6
James <jaimersonaraujo@gmail.com>
2016-05-10 21:30:46 -0300

    Create the cop and add basic specs

--- lib/rubocop/cop/eql/equal_operator_instead_of_eql.rb
+++ lib/rubocop/cop/eql/equal_operator_instead_of_eql.rb
@@ -2,7 +2,14 @@ module RuboCop
   module Cop
     module Eql
       class EqualOperatorInsteadOfEql < Cop
-        
+        MESSAGE = 'Avoid using `==`. Use `eql?` instead.'.freeze
+
+        def on_send(node)
+          _receiver, method_name, _args = *node
+          return unless method_name.eql?(:==)
+
+          add_offense(node, :expression, MESSAGE)
+        end
       end
     end
   end
--- spec/rubocop/cop/eql/equal_operator_instead_of_eql_spec.rb
+++ spec/rubocop/cop/eql/equal_operator_instead_of_eql_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe RuboCop::Cop::Eql::EqualOperatorInsteadOfEql do
+  subject(:cop) { described_class.new }
 
+  it 'detects use of == as an offense' do
+    inspect_source(cop, '1 == 2')
+    expect(cop.messages).to eq(['Avoid using `==`. Use `eql?` instead.'])
+  end
 end
