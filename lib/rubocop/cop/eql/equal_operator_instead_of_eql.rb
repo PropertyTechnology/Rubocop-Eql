948d8fa812000804fb5beebe51f736d6055320cf
Bilal Khan <bilal@advancetogo.com>
2016-05-13 20:42:08 +0100

    Improve error message

--- lib/rubocop/cop/eql/equal_operator_instead_of_eql.rb
+++ lib/rubocop/cop/eql/equal_operator_instead_of_eql.rb
@@ -2,7 +2,7 @@ module RuboCop
   module Cop
     module Eql
       class EqualOperatorInsteadOfEql < Cop
-        MESSAGE = 'Avoid using `==`. Use `eql?` instead.'.freeze
+        MESSAGE = 'Avoid using `==`. Use `.eql?` instead.'.freeze
 
         def on_send(node)
           _receiver, method_name, _args = *node
