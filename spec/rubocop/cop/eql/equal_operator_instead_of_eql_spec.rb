21706e35eaa333d15dd8f934d2b06c5aebd889e4
Bilal Khan <bilal@advancetogo.com>
2016-05-18 11:09:36 +0100

    Repair Rspec errors

--- spec/rubocop/cop/eql/equal_operator_instead_of_eql_spec.rb
+++ spec/rubocop/cop/eql/equal_operator_instead_of_eql_spec.rb
@@ -5,7 +5,7 @@ describe RuboCop::Cop::Eql::EqualOperatorInsteadOfEql do
 
   it 'detects use of == as an offense' do
     inspect_source(cop, '1 == 2')
-    expect(cop.messages).to eq(['Avoid using `==`. Use `eql?` instead.'])
+    expect(cop.messages).to eq(['Avoid using `==`. Use `.eql?` instead.'])
     expect(cop.highlights).to eq(['1 == 2'])
   end
 end
